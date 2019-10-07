54af63cbd8015f4323120037fe6007a99346e917
Drew Merrill <drewcmerrill@gmail.com>
2019-10-07 15:52:08 -0700

    Initial commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,63 @@
+/**
+ * @Author: Drew Merrill 1213000501
+ * Class ID: RD7
+ * Assignment: 2
+ * File Description: This files creates an AddingMachine class with a 
+ * 					 constructor and methods to add and subract numbers
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	/**
+	 * Constructor for the AddingMachine class
+	 */
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns total
+	 * 
+	 * @return 0
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/**
+	 * Add method
+	 * 
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtract method
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 *  toString method
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * clear method
+	 */
+	public void clear() {
+		
+	}
+
+}
